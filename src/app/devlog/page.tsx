80dc6ad5115bbcae0e7ae98cbd0fb939cdf6c59c
황규민 <hwang444493@klic.co.kr>
2025-10-31 18:04:44 +0900

    Refactor layout and styling by removing globals.css, updating layout.tsx for a new header structure, and modifying page.tsx to display a simple title. Update package.json and yarn.lock to include new dependencies and versions.

---
+++ src/app/devlog/page.tsx
@@ -0,0 +1,8 @@
+export default function Devlog() {
+  return (
+    <>
+      <h2>개발 기록</h2>
+      <p>보드 형식으로 글 목록을 보여줄 예정입니다.</p>
+    </>
+  );
+}
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,8 @@
+export default function Portfolio() {
+  return (
+    <>
+      <h2>포트폴리오</h2>
+      <p>갤러리 형식으로 글 목록을 보여줄 예정입니다.</p>
+    </>
+  );
+}
